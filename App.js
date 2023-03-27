351a5e41ac393e9abbd4bdb1e1e3f7a4ef06c487
yashlimbasiya <softwareengineer@limbasiyas-MacBook-Pro.local>
2023-03-27 10:48:16 -0400

    episode-3

--- App.js
+++ App.js
@@ -3,15 +3,29 @@ import React from "react";
 import ReactDOM from"react-dom/client";
 
 const heading = React.createElement("h1",{id:"heading"}, "Hello World From react");
-const tree = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const Jsxheading = () => 
+(<h1 className="head" tabIndex="5"> 
+Namaste React Using JSX
+</h1>);
+
+const HeadingComponent = () => 
+(<div id="conatiner">
+    <h1>
+<Jsxheading/>
+JSX Heading</h1>
+</div>)
 
 
-const parent = React.createElement("div",
-{id:"parent"}, [React.createElement("div",{id:"child"},
-[React.createElement("h1",{},"i m a h1 tag"),React.createElement("h2",
-{},"i m a h2 tag")]),React.createElement("div",{id:"child2"},
-[React.createElement("h1",{},"i m a h1 tag"),
-React.createElement("h2",{},"i m a h2 tag")])]);
 
-console.log(parent);
-tree.render(parent);
+
+// const parent = React.createElement("div",
+// {id:"parent"}, [React.createElement("div",{id:"child"},
+// [React.createElement("h1",{},"i m a h1 tag"),React.createElement("h2",
+// {},"i m a h2 tag")]),React.createElement("div",{id:"child2"},
+// [React.createElement("h1",{},"i m a h1 tag"),
+// React.createElement("h2",{},"i m a h2 tag")])]);
+
+console.log(Jsxheading);
+root.render(<HeadingComponent/>);
